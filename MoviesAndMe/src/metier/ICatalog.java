6be9f03433f9a1a16e301f6eff724110b001d2ae
MorganWolf <43387672+MorganWolf@users.noreply.github.com>
2018-12-16 21:45:22 +0100

    Add files via upload

--- MoviesAndMe/src/metier/ICatalog.java
+++ MoviesAndMe/src/metier/ICatalog.java
@@ -1,17 +1,34 @@
 package metier;
 
 import java.util.List;
+import java.util.Set;
+
+import javax.ws.rs.QueryParam;
 
 import metier.entities.*;
 
 public interface ICatalog {
+	
 	public Film createMovie(Film f);
 	public Cinema createCinema(Cinema c);
-	public void addSeance(Seance s, Cinema c);
+	
+	
+	
+	/* GET */
 	public List<Film> listFilm();
 	public Film FilmById(int id);
 	public List<Film> ListFilmByKeyWord(String keyword);
+	public Set<Film> ListFilmByCity(String city);
+	
+	
 	public List<Cinema> listCinema();
 	public Cinema CinemaById(int id);
+	public List<Cinema> ListCinemaByCity(String city);
+	
+	/* POST */
+	
+	public Film addFilm(Film f);
+	public Seance addSeance(Seance s, Cinema c);
+	
 	
 }
